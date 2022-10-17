d44c8d179558f1df97ad34db891ed483e6d02a50
Pablo Causa <causapablo@gmail.com>
2022-10-17 19:23:26 -0300

    Boilerplate

--- ActorMovies.js
+++ ActorMovies.js
@@ -1,5 +1,8 @@
 const { DataTypes } = require('sequelize');
-
+//Aqui tendriamos que importar los modelos de Sequelize "Movie" y "Actor"
+//Para que este modelo pueda tomar las referencias. 
+//De la instancia de sequelize configurada en el server, let sequelize = new Sequelize("Connection String") pordemos obtener los modelos que se necesitan aca.
+//const {Person, Movie} = require(')
 module.exports = (sequelize) => {
     sequelize.define('ActorxMovie', {
         MovieId: {
