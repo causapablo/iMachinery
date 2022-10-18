22c88be8f9214a4ce859486296177e3b4e5f3189
Pablo Causa <causapablo@gmail.com>
2022-10-17 21:59:31 -0300

    Last detail.

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,20 +8,8 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.get('/pokemon', async (req, res) => {
-    let { asyncMethod } = req.query;
-    if(asyncMethod==="promise"){
-        axios.get('https://pokeapi.co/api/v2/pokemon/?limit=1126')
-        .then(results => res.send(results.data.results))
-        .catch(error => console.log(error));
-    }else if(asyncMethod==="asyncawait"){
-        try{
-            const pokemons = await axios.get("https://pokeapi.co/api/v2/pokemon/?limit=1126");
-            res.send(pokemons.data.results);
-        }catch(error){
-            res.send(error);
-        }
-    }
+router.get('/etc', async (req, res) => {
+    res.send('Hello World');
 })
 
 module.exports = router;
